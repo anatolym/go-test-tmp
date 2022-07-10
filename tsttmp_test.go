1fbdba66f21492c0411acbe7b5573fdc70b7bcbb
Anatoly Milkov <amilkov@goguardian.com>
2022-07-10 09:55:33 -0700

    feat: implement dec

--- tsttmp_test.go
+++ tsttmp_test.go
@@ -24,3 +24,23 @@ func TestInc(t *testing.T) {
 		})
 	}
 }
+
+func TestDec(t *testing.T) {
+	tests := []struct {
+		name string
+		i    int
+		want int
+	}{
+		{"inc 0", 0, -1},
+		{"inc 1", 1, 0},
+		{"inc 2", 2, 1},
+		{"inc 3", 3, 2},
+		{"inc -1", -1, -2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Dec(tt.i)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
