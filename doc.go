ad4b9ade4d1f651700d4f9e0dbae240841789934
Anatoly Milkov <amilkov@goguardian.com>
2022-07-10 09:46:17 -0700

    fix package comment

--- doc.go
+++ doc.go
@@ -1,8 +1,7 @@
-// Package tsttmp is a test package to try out different tooling for maintaining
-// open source projects.
+// Package gotesttmp helps to try out different tooling for maintaining open
+// source projects.
 //
 // - for documentation - pkg.go.dev
 // - for CI - github actions
 // - code coverage - ?
-
 package gotesttmp
